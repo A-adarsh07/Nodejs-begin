1ef4d516409e7bc6fb9c821b778d6f9ba26c7f51
A-adarsh07 <addy.mishra10@gmail.com>
2024-06-17 01:25:38 +0530

    updated

--- http.js
+++ http.js
@@ -4,3 +4,49 @@ http.createServer((req,resp) => {
     resp.write("hello ,I'M Jose Mourihno");
     resp.end(); // to tell browser engine to stop
 }).listen(4500);
+
+
+const fs = require('fs');
+
+// Synchronous Operation: writeFileSync performs the file write operation synchronously, meaning it blocks the execution of the program until the operation completes.
+
+const fs = require('fs');
+
+try {
+  fs.writeFileSync('example.txt', 'Hello, world!');
+  console.log('File written successfully');
+} catch (err) {
+  console.error('Error writing file:', err);
+}
+
+// Asynchronous Operation: 'writeFile' performs the file write operation asynchronously, meaning it does not block the execution of the program. Instead, it uses callbacks or Promises to handle the completion of the operation.
+
+const fs = require('fs').promises;
+
+fs.writeFile('example.txt', 'Hello, world!')
+  .then(() => {
+    console.log('File written successfully');
+  })
+  .catch((err) => {
+    console.error('Error writing file:', err);
+  });
+
+// USAGE WITH ASYNC-AWAIT
+  const fs = require('fs').promises;
+
+async function writeFile() {
+  try {
+    await fs.writeFile('example.txt', 'Hello, world!');
+    console.log('File written successfully');
+  } catch (err) {
+    console.error('Error writing file:', err);
+  }
+}
+
+writeFile();
+
+
+fs.unlink('hello.txt',function(err){
+    if(err) console.log(err);
+    else console.log('removed');
+})
