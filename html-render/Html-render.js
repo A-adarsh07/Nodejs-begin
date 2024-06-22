700c430e3d2c9a05693022abaa3945a27de98e1d
A-adarsh07 <addy.mishra10@gmail.com>
2024-06-22 18:04:45 +0530

    404 file and render file without .html extension

--- html-render/Html-render.js
+++ html-render/Html-render.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const path= require('path');
 const app = express();
-const fs = require('fs');
+
 // access html folder and file
 // console.log(__dirname);
 const publicpath= path.join(__dirname,'public');
 console.log(publicpath);
 
 
-
-
-app.use(express.static(publicpath));
+// app.use(express.static(publicpath));
 // This is a built-in middleware function in Express. It serves static files and is based on serve-static.
+
+// Render multiple html files without url
+app.get('',(req,resp)=>{
+    resp.sendFile(`${publicpath}/index.html`);
+})
+app.get('/aboutme',(req,resp)=>{
+    resp.sendFile(`${publicpath}/about.html`);
+})
+app.get('/help',(req,resp)=>{
+    resp.sendFile(`${publicpath}/help.html`);
+})
+
+app.get('*',(req,resp) => {
+    resp.sendFile(`${publicpath}/404.html`);
+})
+
 app.listen(5000,() => {
     console.log('server running on port 5000');
 });
+
