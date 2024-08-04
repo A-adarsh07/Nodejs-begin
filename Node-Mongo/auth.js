1128e69db65aa5d8f121c691b3081f83c2664a47
A-adarsh07 <addy.mishra10@gmail.com>
2024-08-04 19:31:19 +0530

    created auth.js file and moved the authentication related things

---
+++ Node-Mongo/auth.js
@@ -0,0 +1,26 @@
+// using Passport for Authorization
+const passport = require("passport");
+const localStrategy = require("passport-local").Strategy; //used for username and passport based authentication
+const Person = require('./Models/personSchema');
+
+// username & password verification function
+passport.use(new localStrategy(async (username, password, done) => {
+    try {
+      console.log("Received credentials", username, password);
+      const user =await Person.findOne({ username: username }); // here we are comparing the username provided in person schema
+      if (!user)
+        return done(null, false, { message: "Incorrect username" }); // done is a callback function and it only takes three parameters :done(error,user,info) if user found then error=null,user=true
+
+      const isPasswordMatch= user.password === password ? true:false;
+      if(isPasswordMatch){
+        return done(null,user);
+      } else{
+        return done(null,false,{message:'Incorrect password'});
+      }
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
+
+module.exports=passport;
--- Node-Mongo/server.js
+++ Node-Mongo/server.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const app = express();
 const db = require("./db"); //database
+const passport = require("./auth");
+
 const bodyparser = require("body-parser");
-require('dotenv').config();
-
-// using Passport for Authorization
-const passport = require('passport');
-const localStrategy= require('passport-local').Strategy; //used for username and passport based authentication
-
-const Person = require('./Models/personSchema'); // for getting the username and password details 
+require("dotenv").config();
 
 //Bodyparser is a middleware libreary for Express.js
 // It is used to parse and extract the body of incoming HTTP requests
@@ -17,36 +13,24 @@ app.use(bodyparser.json());
 const PORT = process.env.PORT || 3000;
 
 // Middleware Function
-const logRequest= (req,res,next)=> {
-  console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
-  next();  //Move on to next phase i.e it'll now move to server to get data 
-}
+const logRequest = (req, res, next) => {
+  console.log(
+    `[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`
+  );
+  next(); //Move on to next phase i.e it'll now move to server to get data
+};
 app.use(logRequest);
 
-// username & password verification function 
-app.use(new localStrategy(async(username,password,done) => {
-  try {
-    console.log('Received credentials',username,password);
-    const user = Person.findOne({username:username}); // here we are comparing the username provided in person schema 
-    if(!username)
-      return done(null,false,{message:'Incorrect username'}) ;  // done is a callback function and it only takes three parameters :done(error,user,info) if user found then error=null,user=true 
+app.use(passport.initialize()); //The code app.use(passport.initialize()); is used in an Express.js application to integrate Passport.js, a popular authentication middleware for Node.js.
+const localAuthMiddleware = passport.authenticate("local", { session: false });
 
-    
+// Import the router files
+const personRoutes = require("./routes/personroutes");
+// Use the routers
+app.use("/person", localAuthMiddleware, personRoutes); // deleted the person from router file and use here , used Local authentication for person
 
-  } catch (error) {
-    
-  }
-})
-)
-
-
-// Import the router files 
-        const personRoutes = require('./routes/personroutes');
-// Use the routers 
-        app.use('/person',personRoutes); // deleted the person from router file and use here
-
-    const menuRoutes= require('./routes/menuroutes');
-    app.use('/menu',menuRoutes);
+const menuRoutes = require("./routes/menuroutes");
+app.use("/menu", menuRoutes);
 
 app.get("/", function (req, res) {
   res.send("welcome to the restaurant");
